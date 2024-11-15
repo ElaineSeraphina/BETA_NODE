6158020c7ba63d2d59d0591fa3f883fc9b393dcd
elaineseraphina <93545091+ElaineSeraphina@users.noreply.github.com>
2024-11-15 15:51:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ RETRIES = 60  # Global retry counter for ping failures
 
 DOMAIN_API = {
     "SESSION": "https://api.nodepay.ai/api/auth/session",
-    "PING": "https://nw2.nodepay.ai/api/network/ping"
+    "PING": [
+        "http://52.77.10.116/api/network/ping",
+        "http://13.215.134.222/api/network/ping",
+        "http://18.136.143.169/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://18.142.214.13/api/network/ping",
+        "http://18.142.29.174/api/network/ping",
+        "http://52.74.31.107/api/network/ping"
+    ]
 }
 
 CONNECTION_STATES = {
