fbfcfd4f7b7c9f67ccf1b3e12e620d72cd4896f6
elaineseraphina <93545091+ElaineSeraphina@users.noreply.github.com>
2024-11-16 03:18:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import sys
 import logging
 
 logging.disable(logging.ERROR)
-from utils.config import DOMAIN_API
 
 # Initialize colorama
 init(autoreset=True)
@@ -41,6 +40,20 @@ status_connect = CONNECTION_STATES["NONE_CONNECTION"]
 browser_id = None
 account_info = {}
 last_ping_time = {}
+
+# Define DOMAIN_API directly in the script
+DOMAIN_API = {
+    "SESSION": "https://api.nodepay.ai/api/auth/session",
+    "PING": [
+        "http://52.77.10.116/api/network/ping",
+        "http://13.215.134.222/api/network/ping",
+        "http://18.136.143.169/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://18.142.214.13/api/network/ping",
+        "http://18.142.29.174/api/network/ping",
+        "http://52.74.31.107/api/network/ping"
+    ]
+}
 
 def uuidv4():
     return str(uuid.uuid4())
